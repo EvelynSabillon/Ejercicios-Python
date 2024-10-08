9bc2c09b1ccb0007352297110ed3ca37739b9fee
Evelyn Sabillon <evelyn.sabillon@unah.hn>
2024-10-08 01:00:51 -0600

    Agregando try except a los programitas

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,12 +1,18 @@
 class Rectangulo:
     def __init__(self, base, altura):
-        self.base = base
-        self.altura = altura
+        try:
+            self.base = float(base)
+            self.altura = float(altura)
+        except ValueError as e:  # El ValueError es un tipo de excepcion que se lanza cuando se intenta convertir un valor a un tipo numerico y no se puede
+            print('Error: Los valores deben ser numericos')
+            print(e)
+            exit()
+
     def area(self):
         return self.base * self.altura
     def perimetro(self):
         return (2 * self.base) + (2 * self.altura)
 
-rectangulo1 = Rectangulo(5, 3)
+rectangulo1 = Rectangulo("s", 3)
 print(f'El area del rectangulo es: {rectangulo1.area()}')
 print(f'El perimetro del rectangulo es: {rectangulo1.perimetro()}')
